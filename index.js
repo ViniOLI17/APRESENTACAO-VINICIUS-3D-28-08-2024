00c8bed53616a262f20962b9614cd33eab0577eb
ViniOLI17 <127845064+ViniOLI17@users.noreply.github.com>
2024-08-08 01:34:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const primeiroNome = "Vinícius";
+const sobrenome = "Oliveira";
+const idade = 17;
+const altura = 1.72;
+const massa = 53;
+const cidadeNatal = "Curitiba";
+
+console.log("O meu primeiro nome é")
+console.log(primeiroNome);
+console.log("A minha idade é");
+console.log(idade);
+console.log("A minha altura é");
+console.log(altura);
+console.log("A minha massa é");
+console.log(massa);
+console.log("Eu nasci em");
+console.log(cidadeNatal);
